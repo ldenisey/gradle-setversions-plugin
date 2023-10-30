350358b82146a955a38918568563797048cf2f58
ldenisey <ldenisey@gmail.com>
2023-10-30 09:58:37 +0100

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,13 @@ repositories {
 
 plugins {
     `java-gradle-plugin`
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.9.10"
 
-    id("com.github.ldenisey.setversions") version "1.0.2"
+    id("com.github.ldenisey.setversions") version "1.0.3"
     id("org.jetbrains.changelog") version "2.0.0"
 
     id("maven-publish")
-    id("com.gradle.plugin-publish") version "1.1.0"
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 group = "com.github.ldenisey"
@@ -20,8 +20,8 @@ version = "1.0.4-SNAPSHOT"
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.8.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.9.10")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter")
     testImplementation(gradleTestKit())
 }
